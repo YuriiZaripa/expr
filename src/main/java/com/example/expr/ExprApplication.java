810a559a79250341d99ce78b099769cc7212ebef
YuriiZaripa <0939864774z@gmail.com>
2021-12-30 19:33:03 +0200

    Initial commit

---
+++ src/main/java/com/example/expr/ExprApplication.java
@@ -0,0 +1,13 @@
+package com.example.expr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExprApplication.class, args);
+    }
+
+}
