fa737d68605cd627b0f7c37a6e0db712cb97fc6f
YuriiZaripa <0939864774z@gmail.com>
2024-01-12 18:44:11 +0200

    updated spring boot version to 3.2.1

--- src/main/java/ua/okwine/productexpirationdate/entity/Product.java
+++ src/main/java/ua/okwine/productexpirationdate/entity/Product.java
@@ -1,9 +1,19 @@
 package ua.okwine.productexpirationdate.entity;
 
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.UUID;
 
--- src/main/java/ua/okwine/productexpirationdate/entity/Supplier.java
+++ src/main/java/ua/okwine/productexpirationdate/entity/Supplier.java
@@ -1,11 +1,17 @@
 package ua.okwine.productexpirationdate.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
