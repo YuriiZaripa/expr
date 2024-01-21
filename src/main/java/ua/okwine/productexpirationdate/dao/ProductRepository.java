a4252e7a8f83b9e985499e88690516559bd3bc51
YuriiZaripa <0939864774z@gmail.com>
2024-01-21 03:20:15 +0200

    added okwine parser

--- src/main/java/ua/okwine/productexpirationdate/dao/ProductRepository.java
+++ src/main/java/ua/okwine/productexpirationdate/dao/ProductRepository.java
@@ -17,4 +17,6 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
     List<Product> findAllNotReportedByAdvanceNotice(@Param("type") String type);
 
     List<Product> findByIsReportedFalse();
+
+    List<Product> findByImageIsNull();
 }
